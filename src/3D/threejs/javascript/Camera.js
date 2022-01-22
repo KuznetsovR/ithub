f82d9ee0f3be46c6192b973bbb203f0c7ca00ee0
KuznetsovR <abcruslan2004@mail.ru>
2022-01-22 16:23:18 +0500

    created camera

--- src/3D/threejs/javascript/Camera.js
+++ src/3D/threejs/javascript/Camera.js
@@ -1,3 +1,21 @@
-export default class Camera{
+import Experience from './Experience';
+import * as THREE from 'three';
 
+export default class Camera {
+  experience = new Experience();
+  sizes = this.experience.sizes;
+  scene = this.experience.scene;
+  canvas = this.experience.canvas;
+  constructor() {
+    this.setInstance()
+  }
+  setInstance() {
+    this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100);
+    this.instance.position.set(6, 4, 8);
+    this.scene.add(this.instance);
+  }
+  resize(){
+    this.instance.aspect = this.sizes.width / this.sizes.height
+    this.instance.updateProjectionMatrix()
+  }
 }
