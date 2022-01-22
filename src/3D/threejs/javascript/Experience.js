b8487e5410744be0c8078b8406adec641018c501
KuznetsovR <abcruslan2004@mail.ru>
2022-01-22 16:25:21 +0500

    added creation of everything needed in main three js class

--- src/3D/threejs/javascript/Experience.js
+++ src/3D/threejs/javascript/Experience.js
@@ -1,12 +1,20 @@
-let instance = null
+import * as THREE from 'three';
+import Sizes from './Utils/Sizes';
+import Camera from "./Camera";
+import Renderer from "./Renderer";
 
-export default class Experience{
-    constructor(canvas) {
-        if (instance){
-            return instance
-        }
-        instance = this
-        this.canvas = canvas
+let instance = null;
+
+export default class Experience {
+  constructor(canvas) {
+    if (instance) {
+      return instance;
     }
-
+    instance = this;
+    this.canvas = canvas
+    this.sizes = new Sizes()
+    this.scene = new THREE.Scene()
+    this.camera = new Camera()
+    this.renderer = new Renderer()
+  }
 }
