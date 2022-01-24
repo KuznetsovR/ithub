d4f19cb45772fa42076e17769000e5d2e8bd4875
KuznetsovR <abcruslan2004@mail.ru>
2022-01-24 18:12:56 +0500

    added main page

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
 import React from 'react';
 import { Navigation3d } from './3D/navigation3d';
+import { MainPage } from './pages/MainPage/MainPage';
 
 export const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Navigation3d />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/navigation" element={<Navigation3d />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
