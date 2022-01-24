b2d19b2f3350cf08c8b7a7b7bf932cebdda26e24
KuznetsovR <abcruslan2004@mail.ru>
2022-01-24 18:27:09 +0500

    added CoursesPage

---
+++ src/pages/CoursesPage/CoursesPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CoursesPage.scss'
+
+export const CoursesPage = () => {
+    return (
+        <div>
+            Страница курсов работает
+        </div>
+    )
+}
