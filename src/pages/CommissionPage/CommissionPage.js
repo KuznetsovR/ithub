3392185d2f41816a8100e89b75d40e125c3eae2b
KuznetsovR <abcruslan2004@mail.ru>
2022-01-24 18:27:19 +0500

    added commission page

---
+++ src/pages/CommissionPage/CommissionPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CommissionPage.scss'
+
+export const CommissionPage = () => {
+    return(
+        <div>
+            Страница priemnoi комиссии тут
+        </div>
+    )
+}
