84f275eec955a0041432f82ed0c07f2ef431ea01
KuznetsovR <abcruslan2004@mail.ru>
2022-01-31 14:57:43 +0500

    fixed main page

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -6,12 +6,8 @@ import { Header } from '../../components/Header/Header';
 export const MainPage = () => {
   return (
     <main>
-      <Header></Header>
-      <HexaButton onClick={}>123</HexaButton>
-      <HexaButton onClick={}>123</HexaButton>
-      <HexaButton onClick={}>123</HexaButton>
-      <HexaButton onClick={}>123</HexaButton>
-      <HexaButton onClick={}>123</HexaButton>
+      <Header />
+      <HexaButton onClick={() => console.log(123)}>123</HexaButton>
     </main>
   );
 };
