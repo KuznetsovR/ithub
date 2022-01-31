d3180b7efd62fac27b2eb68b416de90639bde2ad
Polylykay <rubtsova-tanyal@mail.ru>
2022-01-31 23:45:07 +0500

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,18 @@ import './Header.scss';
 
 export const Header = () => {
   return (
-      <header>SOme cod e in header </header>
+      <header>
+         <img src="./img/itlogo.png" class="logoheader" alt="logo"> </img>
+      <div class="rectangle2"></div>
+      
+      <div class="rectangle">
+        <div class="navig">
+        <a class="martex">Приёмная комиссия </a>
+        <a class="martex">Специальности</a>
+        <a class="">Курсы</a>
+        </div>
+        
+      </div>
+       </header>
   );
 };
