f77b5ef49a58eec571b8211bab54d4d18b4b96a4
NastyaFlex <homa2003133@gmail.com>
2022-02-02 01:40:07 +0500

    Code reorganisation. Kebab-case.

--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import 'Phone.scss';
+import './Phone.scss';
 
 export const Phone = () => {
   return (
-    <div class="phone_service_support">
-      <p class="help">Появились вопросы? Позвоните нам!<span class="number"> +7 777 666 33 33</span></p>
+    <div className="phone-service-support">
+      <p className="help">Появились вопросы? Позвоните нам!<span className="number"> +7 777 666 33 33</span></p>
     </div>
   );
 };
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,16 +2,13 @@ import React from 'react';
 import './MainPage.scss';
 import { HexaButton } from '../../components/HexaButton/HexaButton';
 import { Header } from '../../components/Header/Header';
-
+import { Phone } from '../../components/Phone/Phone';
 export const MainPage = () => {
   return (
     <main>
-      <Header></Header>
-      <HexaButton onClick={}>123</HexaButton>
-      <HexaButton onClick={}>123</HexaButton>
-      <HexaButton onClick={}>123</HexaButton>
-      <HexaButton onClick={}>123</HexaButton>
-      <HexaButton onClick={}>123</HexaButton>
+      <Header />
+      <HexaButton onClick={() => console.log(123)}>123</HexaButton>
+      <Phone />?
     </main>
   );
 };
