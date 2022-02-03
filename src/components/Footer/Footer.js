12ad308c01712c807e8597a76f893d6db2a44c56
Polylykay <rubtsova-tanyal@mail.ru>
2022-02-03 20:16:25 +0500

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './Footer.scss';
-import logo from '../../images/IThublogo.png' 
+import logo from '../../assets/images/IThublogo.png' 
 
 export const Footer = () => {
   return ( <footer>
-      
+    <div className="container">
+<div className="footertext"> <img src={logo} className="logofooter" alt="logoit" />
+ <a href="" className="textaddress"> Екатеринбург, Чкалова 3</a>
+ <a href="" className="textcopyright">© Copyright 2021 ITHub Ekat college</a></div>
+<div className="footertext"> <a href="" className="text3d"> ITJourney 3D</a> </div>
+<div className="footertext">  <a href="" className="textcontacts"> Контакты</a> 
+<a href="" className="textemail"> &#9993; priem.ekat@ithub.ru </a>
+<a href="" className="textemail textphone" > &#9742; +7 (343) 286 78 59</a> 
+</div>
+</div>
   </footer>);
 };
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './MainPage.scss';
 import { HexaButton } from '../../components/HexaButton/HexaButton';
 import { Header } from '../../components/Header/Header';
-
+import { Footer } from '../../components/Footer/Footer';
 export const MainPage = () => {
   return (
     <main>
       <Header />
       <HexaButton onClick={() => console.log(123)}>123</HexaButton>
+      <Footer />
     </main>
   );
 };
