cce72d986935c8f0c671160966d7003a13ac46c6
BogdanDjun <i21s045@students.it-college.ru>
2022-02-04 00:03:55 +0500

    first version of Specialties (fixed)

--- src/components/Specialties/Specialties.js
+++ src/components/Specialties/Specialties.js
@@ -4,25 +4,17 @@ import './Specialties.scss';
 export const Specialties = () => {
   return (
     <div className="d-flex container" id="cont1">
-      <div className="specsText">
-        <h2>Специальности:</h2>
-      </div>
+      <div className="specsText">Специальности:</div>
       <div className="specs">
-        {/* <div className="Content1">
-          <div className="rectangleP1" />
-          <div className="picture1" />
-          <div className="textOnPic1" />
-          <h3 className="text1">Веб-Разработчик</h3>
-        </div> */}
-        <div className="rectangleP2">
-          <div className="text2">Веб-Разработчик</div>
+        <div className="rectangleP1">
+          <div className="text1">Веб-Разработчик</div>
         </div>
-        {/* <div className="Content3">
-          <div className="rectangleP3" />
-          <div className="picture3" />
-          <div className="textOnPic3" />
-          <h3 className="text2">Веб-Разработчик</h3>
-        </div> */}
+        <div className="rectangleP2">
+          <div className="text2">Программист</div>
+        </div>
+        <div className="rectangleP3">
+          <div className="text3">Системный админимтсратор</div>
+        </div>
       </div>
     </div>
   );
