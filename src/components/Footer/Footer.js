8a370d2feb41a7cd8ab6850aa863f13102759515
Polylykay <rubtsova-tanyal@mail.ru>
2022-02-03 21:12:46 +0500

    new footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,10 +7,8 @@ export const Footer = () => {
     <footer>
       <div className="container">
         <div className="footertext">
-          {' '}
           <img src={logo} className="logofooter" alt="logoit" />
           <a href="" className="textaddress">
-            {' '}
             Екатеринбург, Чкалова 3
           </a>
           <a href="" className="textcopyright">
@@ -18,24 +16,18 @@ export const Footer = () => {
           </a>
         </div>
         <div className="footertext">
-          {' '}
           <a href="" className="text3d">
-            {' '}
             ITJourney 3D
-          </a>{' '}
+          </a>
         </div>
         <div className="footertext">
-          {' '}
           <a href="" className="textcontacts">
-            {' '}
             Контакты
           </a>
           <a href="" className="textemail">
-            {' '}
-            &#9993; priem.ekat@ithub.ru{' '}
+            &#9993; priem.ekat@ithub.ru
           </a>
           <a href="" className="textemail textphone">
-            {' '}
             &#9742; +7 (343) 286 78 59
           </a>
         </div>
