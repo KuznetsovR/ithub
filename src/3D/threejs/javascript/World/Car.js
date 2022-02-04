89137a87985a394273825a391ad633f69ca53ded
KuznetsovR <abcruslan2004@mail.ru>
2022-02-04 14:14:46 +0500

    created car and controls files for 3d

---
+++ src/3D/threejs/javascript/World/Car.js
@@ -0,0 +1,5 @@
+import Experience from '../Experience';
+
+export default class Car {
+  experience = new Experience()
+}
---
+++ src/3D/threejs/javascript/World/Controls.js
@@ -0,0 +1,6 @@
+import EventEmitter from '../Utils/EventEmitter';
+import Experience from '../Experience';
+
+export default class Controls extends EventEmitter  {
+  experience = new Experience()
+}
