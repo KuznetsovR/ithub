bdba580f350940fa4437e1cee473fefbd126c9d5
Polylykay <rubtsova-tanyal@mail.ru>
2022-02-07 21:34:25 +0500

    dogs

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -5,7 +5,7 @@ import { Footer } from '../../components/Footer/Footer';
 import { Phone } from '../../components/Phone/Phone';
 import Slider from 'react-slick';
 import { SpecialityModal } from '../../components/SpecialityModal/SpecialityModal';
-import { Documents } from '../../components/Documents/Documents.JS';
+import { Documents } from '../../components/Documents/Documents';
 
 export const MainPage = () => {
   const settings = {
@@ -19,46 +19,49 @@ export const MainPage = () => {
     // pauseOnHover: true,
     arrows: false,
   };
-  return (
-    <main>
-      <Header />
-
-      <Phone />
-
-      <SpecialityModal>
-        <div>asdjkfnoewiuipdinowevbieghejpfmwvni</div>
-      </SpecialityModal>
-
-      <div className="slider-wrapper">
-        <Slider {...settings}>
-          <div className={'slide'}>
-            <img
-              className={'slide-img'}
-              src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
-              alt=""
-            />
-            <div className="description">some text</div>
+    return (
+      <>
+        <Header />
+        <main>
+          <Phone />
+  
+          <div className="container">
+            <SpecialityModal>
+              <div>asdjkfnoewiuipdinowevbieghejpfmwvni</div>
+            </SpecialityModal>
+  
+            <div className="slider-wrapper">
+              <Slider {...settings}>
+                <div className={'slide'}>
+                  <img
+                    className={'slide-img'}
+                    src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
+                    alt=""
+                  />
+                  <div className="description">some text</div>
+                </div>
+                <div className={'slide'}>
+                  <img
+                    className={'slide-img'}
+                    src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
+                    alt=""
+                  />
+                  <div className="description">some text</div>
+                </div>
+                <div className={'slide'}>
+                  <img
+                    className={'slide-img'}
+                    src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
+                    alt=""
+                  />
+                  <div className="description">some text</div>
+                </div>
+              </Slider>
+            </div>
+            < Documents />
           </div>
-          <div className={'slide'}>
-            <img
-              className={'slide-img'}
-              src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
-              alt=""
-            />
-            <div className="description">some text</div>
-          </div>
-          <div className={'slide'}>
-            <img
-              className={'slide-img'}
-              src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
-              alt=""
-            />
-            <div className="description">some text</div>
-          </div>
-        </Slider>
-      </div>
-      <Documents />
-      <Footer />
-    </main>
-  );
+        </main>
+        <Footer />
+      </>
+    );
 };
