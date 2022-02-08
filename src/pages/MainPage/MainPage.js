91ed21055779757f0fbf63eb0f8df1332bb1c57f
KuznetsovR <abcruslan2004@mail.ru>
2022-02-08 15:27:53 +0500

    moved slider from main to open day page

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -3,22 +3,10 @@ import './MainPage.scss';
 import { Header } from '../../components/Header/Header';
 import { Footer } from '../../components/Footer/Footer';
 import { Phone } from '../../components/Phone/Phone';
-import Slider from 'react-slick';
-import { SpecialityModal } from '../../components/SpecialityModal/SpecialityModal';
 import { YandexMap } from '../../components/YandexMap/YandexMap';
+import { OpenDay } from '../../components/OpenDay/OpenDay';
 
 export const MainPage = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 1300,
-    autoplaySpeed: 5000,
-    // pauseOnHover: true,
-    arrows: false,
-  };
   return (
     <>
       <Header />
@@ -26,38 +14,7 @@ export const MainPage = () => {
         <Phone />
 
         <div className="container">
-          <SpecialityModal>
-            <div>asdjkfnoewiuipdinowevbieghejpfmwvni</div>
-          </SpecialityModal>
-
-          <div className="slider-wrapper">
-            <Slider {...settings}>
-              <div className={'slide'}>
-                <img
-                  className={'slide-img'}
-                  src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
-                  alt=""
-                />
-                <div className="description">some text</div>
-              </div>
-              <div className={'slide'}>
-                <img
-                  className={'slide-img'}
-                  src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
-                  alt=""
-                />
-                <div className="description">some text</div>
-              </div>
-              <div className={'slide'}>
-                <img
-                  className={'slide-img'}
-                  src="https://getbootstrap.com/docs/5.1/assets/img/bootstrap-icons.png"
-                  alt=""
-                />
-                <div className="description">some text</div>
-              </div>
-            </Slider>
-          </div>
+          <OpenDay />
           <YandexMap />
         </div>
       </main>
