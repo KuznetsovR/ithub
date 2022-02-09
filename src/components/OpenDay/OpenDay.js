52087f311e45df6f9557654315afc8eca2784b4f
KuznetsovR <abcruslan2004@mail.ru>
2022-02-09 12:06:03 +0500

    fixed design

--- src/components/OpenDay/OpenDay.js
+++ src/components/OpenDay/OpenDay.js
@@ -18,8 +18,8 @@ export const OpenDay = () => {
   return (
     <div className="open-day-component">
       <div className="open-day">
-        <h3>День открытых дверей</h3>
         <div className="open-day-content-wrapper">
+        <h3>День открытых дверей</h3>
           <div className="open-day-desc">
             Приглашаем <span className="open-day-focus"> абитуриентов </span> и их
             <span className="open-day-focus"> родителей</span> посетить наш колледж
