e0a5e2481ea55aac169f6420b4f8ffb10aac7c41
KuznetsovR <abcruslan2004@mail.ru>
2022-02-09 12:22:27 +0500

    fixed documents design

--- src/components/Documents/Documents.js
+++ src/components/Documents/Documents.js
@@ -8,9 +8,9 @@ export const Documents = () => {
       <p className="text-title"> Сведения об образовательной организации </p>
       <div className="grid">
         <div className="text-conditions">
-          <p>
+          <div>
             Обучение проходит по Федеральному Государственному Образовательному Стандарту (ФГОС) 09.02.07.
-          </p>
+          </div>
           <p>
             Действуем на основании образовательной лицензии №040965 от «8» сентября 2020 г. Специальности
             прошли государственную аккредитацию (Свид. № от 004965).
@@ -19,7 +19,6 @@ export const Documents = () => {
           <p>Действует отсрочка от армии.</p>
         </div>
         <div>
-         
           <img src={document} className="document-photo" alt="documents" />
         </div>
       </div>
