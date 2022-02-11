e8488b502c9f3e019983056c18f3fb68b4018272
KuznetsovR <abcruslan2004@mail.ru>
2022-02-11 14:08:09 +0500

    fixed everything

--- src/pages/CoursesPage/CoursesPage.js
+++ src/pages/CoursesPage/CoursesPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import './Courses.scss';
+import './CoursesPage.scss'
 import { Courses } from '../../components/Courses/Courses';
+import { Header } from '../../components/Header/Header';
+import { Phone } from '../../components/Phone/Phone';
+import { Footer } from '../../components/Footer/Footer';
 
 export const CoursesPage = () => {
   return (
@@ -8,7 +11,9 @@ export const CoursesPage = () => {
       <Header />
       <main>
         <Phone />
-        <div className="container"></div>
+        <div className="container">
+          <Courses></Courses>
+        </div>
       </main>
       <Footer />
     </>
