1e6ff59d70e3317574a50002dfcc6b3afbb67619
KuznetsovR <abcruslan2004@mail.ru>
2022-03-01 14:18:26 +0500

    added animations

--- src/3D/threejs/javascript/World/MainHero.js
+++ src/3D/threejs/javascript/World/MainHero.js
@@ -22,6 +22,7 @@ export default class MainHero {
     this.setMovement();
     this.setSkate();
     this.setMainHero();
+    this.setAnimations()
   }
   setModel() {
     this.model = {
@@ -84,5 +85,11 @@ export default class MainHero {
       this.mainHero.object.position.copy(this.physics.skate.chassis.body.position).add(this.mainHero.offset);
       this.mainHero.object.quaternion.copy(this.physics.skate.chassis.body.quaternion);
     });
+  }
+  setAnimations(){
+    this.mixer = new THREE.AnimationMixer(this.container)
+    this.mainHeroAction =  this.mixer.clipAction(this.model.mainHero.animations[1])
+    this.mainHeroAction.play()
+    console.log(this.mainHeroAction);
   }
 }
