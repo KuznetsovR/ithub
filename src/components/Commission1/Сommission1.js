e31964ae5466d126ad3b0fab654eb3a85c1f578f
KuznetsovR <abcruslan2004@mail.ru>
2022-03-04 19:03:57 +0500

    fixed document downloading

--- src/components/Commission1/Сommission1.js
+++ src/components/Commission1/Сommission1.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Сommission1.scss';
 import { HexaButton } from '../HexaButton/HexaButton';
+import { Link } from 'react-router-dom';
+import document1 from '@assets/images/document_1.jpg';
+import document2 from '@assets/images/document_2.jpg';
+import document3 from '@assets/images/document_3.jpg';
+import document4 from '@assets/images/document_4.jpg';
 
 export const Commission1 = () => {
   return (
@@ -20,7 +25,10 @@ export const Commission1 = () => {
         </div>
         <div className="comission-column-3">
           <div className="commission-btn-wrapper">
-            <HexaButton>Скачать документы</HexaButton>
+            <Link to={document1} target="_blank" download>Скачать документ 1</Link>
+            <Link to={document2} target="_blank" download>Скачать документ 2</Link>
+            <Link to={document3} target="_blank" download>Скачать документ 3</Link>
+            <Link to={document4} target="_blank" download>Скачать документ 4</Link>
           </div>
         </div>
       </div>
