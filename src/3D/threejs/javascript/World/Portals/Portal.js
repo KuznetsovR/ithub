712c7e774bd26af33d49e9c256e73c2b5ae317ad
KuznetsovR <abcruslan2004@mail.ru>
2022-03-10 17:54:45 +0500

    refactored portals

--- src/3D/threejs/javascript/World/Portals/Portal.js
+++ src/3D/threejs/javascript/World/Portals/Portal.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import EventEmitter from '../../Utils/EventEmitter';
 
 export default class Portal extends EventEmitter {
-  constructor(name,position, url, size, timer) {
+  constructor(name, position, url, size, timer) {
     super();
     this.world = new World();
     this.mainHero = this.world.mainHero;
@@ -12,8 +12,8 @@ export default class Portal extends EventEmitter {
     this.url = url;
     this.position = position;
     this.size = size;
-    this.name = name
-    this.timer = timer
+    this.name = name;
+    this.timer = timer;
 
     this.container = new THREE.Object3D();
     this.container.position.x = this.position.x;
@@ -32,7 +32,7 @@ export default class Portal extends EventEmitter {
 
   setPortal() {
     this.portal = {
-      geometry: new THREE.CircleGeometry(this.size, 6),
+      geometry: new THREE.CircleGeometry(this.size / 2, 6),
       material: new THREE.MeshBasicMaterial({ color: 0x9e3169 }),
     };
     this.portal.mesh = new THREE.Mesh(this.portal.geometry, this.portal.material);
@@ -46,14 +46,23 @@ export default class Portal extends EventEmitter {
         if (this.elapsedTimeInPortal > 100) {
           this.redirect();
         } else if (this.skateIsInArea) {
-          this.timer.setCurrentPortal(this.name)
+          this.timer.setCurrentPortal(this.name);
           this.elapsedTimeInPortal += this.time.delta / 30;
+
+          // console.log(
+          //   'X: ',
+          //   Math.abs(this.mainHero.position.x ),
+          //   Math.abs(this.position.x),
+          //   'Y: ',
+          //   Math.abs(this.mainHero.position.y),
+          //   Math.abs(this.position.y),
+          // );
+
           this.timer.makeTimerVisible();
-          console.log(this.elapsedTimeInPortal, this.timer);
         } else if (this.elapsedTimeInPortal > 0) {
           this.elapsedTimeInPortal -= this.time.delta / 30;
         } else {
-          this.timer.setCurrentPortal(null)
+          this.timer.setCurrentPortal(null);
           this.timer.makeTimerInvisible();
         }
         this.timer.setTimerWidth(this.elapsedTimeInPortal / 1.5);
@@ -66,8 +75,8 @@ export default class Portal extends EventEmitter {
   observeSkate() {
     this.time.on('tick', () => {
       const skateIsIn =
-        Math.abs(this.mainHero.position.x - this.position.x) < Math.abs(this.size / 2) * 1.5 &&
-        Math.abs(this.mainHero.position.y - this.position.y) < Math.abs(this.size / 2) * 1.5;
+        Math.abs(this.mainHero.position.x - this.position.x) < Math.abs(this.size / 2) &&
+        Math.abs(this.mainHero.position.y - this.position.y) < Math.abs(this.size / 2);
       if (skateIsIn !== this.skateIsInArea) {
         this.skateIsInArea = skateIsIn;
       }
