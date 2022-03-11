27a70e0b1a938710c7b1282bea799fa1daec2be3
KuznetsovR <abcruslan2004@mail.ru>
2022-03-11 17:30:28 +0500

    inserted validators in open day controller

--- backend/open-day/open-day-controller.js
+++ backend/open-day/open-day-controller.js
@@ -1,3 +1,7 @@
+const { validateName } = require('../validators/name-validator');
+const { validateEmail } = require('../validators/email-validator');
+const { validatePhone } = require('../validators/phone-validator');
+
 class OpenDayController {
   constructor() {
     this.visitors = [];
@@ -19,16 +23,11 @@ class OpenDayController {
     // use mailer
   }
   validateVisitor(visitor) {
-    const phoneRegex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
-    const emailRegex = new RegExp(
-      '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])'
-    );
-
     if (
       visitor &&
-      typeof visitor.name === 'string' &&
-      phoneRegex.test(visitor.phone) &&
-      emailRegex.test(visitor.email) &&
+      validateName(visitor.name) &&
+      validatePhone(visitor.phone) &&
+      validateEmail(visitor.email) &&
       typeof visitor.date === 'string' &&
       visitor.personalDataAccess === true
     ) return true;
