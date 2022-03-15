7e180f4d66407db8d1bb8d0b11c8d4408ce649a5
KuznetsovR <abcruslan2004@mail.ru>
2022-03-15 20:39:47 +0500

    fixed file validator

--- src/validators/file-validator.js
+++ src/validators/file-validator.js
@@ -3,7 +3,7 @@ const defaultAllowedExtensionsRegex = /(\.doc|\.docx|\.rtf|\.pdf|\.odt|\.jpeg|\.
 export const validateFiles = (files, extensionsRegex) => {
   const regex = extensionsRegex || defaultAllowedExtensionsRegex;
   for (const file of files) {
-    if (!regex.exec(file.originalname)) {
+    if (!regex.exec(file?.name)) {
       return false;
     }
   }
