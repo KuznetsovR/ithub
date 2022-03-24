077166919f59d64bef9b0f46ce5b054513444a34
KuznetsovR <abcruslan2004@mail.ru>
2022-03-24 11:42:09 +0500

    minor fixes and renamed courses to about

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './app.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
-import { MainPage, CoursesPage, CommissionPage } from '@pages/export.pages.js';
+import { MainPage, AboutPage, CommissionPage } from '@pages/export.pages.js';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/courses" element={<CoursesPage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/commission" element={<CommissionPage />} />
       </Routes>
     </BrowserRouter>
--- src/pages/CommissionPage/CommissionPage.js
+++ src/pages/CommissionPage/CommissionPage.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './CommissionPage.scss';
 import { Header, Commission1, Commission2, Footer, Phone } from '../../components/export.components.js';
 export const CommissionPage = () => {
-
   return (
     <>
       <Header />
-          <Phone />
+      <Phone />
       <main>
-        <Phone />
         <div className="container">
           <Commission1 />
           <Commission2 />
