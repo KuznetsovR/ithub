6d112e9e12d79585422ff93fb40143905ca3c2e3
KuznetsovR <abcruslan2004@mail.ru>
2022-03-24 14:49:32 +0500

    changed naming

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './FileUpload.scss';
 
-export const FileUpload = ({ setState }) => {
-  return <input type="file" className={'file-input'} onChange={(e) => setState(e)} />;
+export const FileUpload = (props) => {
+  return <input type="file" className={'file-input'} onChange={(e) => props.onChange(e)} />;
 };
