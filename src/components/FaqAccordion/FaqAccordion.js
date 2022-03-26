d02f0ed1eb381671ecce820c6cdbe23b46b7497a
Polylykay <rubtsova-tanyal@mail.ru>
2022-03-26 17:21:32 +0500

    accordion

--- src/components/FaqAccordion/FaqAccordion.js
+++ src/components/FaqAccordion/FaqAccordion.js
@@ -9,8 +9,12 @@ import Typography from '@mui/material/Typography';
 const Accordion = styled((props) => <MuiAccordion disableGutters elevation={0} square {...props} />)(
   ({ theme }) => ({
     border: `1px solid ${theme.palette.divider}`,
+    borderColor: 'rgba(91,21,115)',
+    borderRadius: '10px',
+    backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(30,29,29)',
     '&:not(:last-child)': {
       borderBottom: 0,
+      color: 'rgba(255,255,255)',
     },
     '&:before': {
       display: 'none',
@@ -21,19 +25,25 @@ const Accordion = styled((props) => <MuiAccordion disableGutters elevation={0} s
 const AccordionSummary = styled((props) => (
   <MuiAccordionSummary expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />} {...props} />
 ))(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(0, 0, 0, .03)',
+  backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(30,29,29)',
+  borderRadius: '10px',
+  borderColor: 'rgba(26,26,26)',
   flexDirection: 'row-reverse',
+  
   '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(90deg)',
+    transform: 'rotate(90deg)',           
+    color: 'rgba(255,255,255)',
   },
   '& .MuiAccordionSummary-content': {
     marginLeft: theme.spacing(1),
+    color: 'rgba(167,29,216)',
   },
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  borderTop: '1px solid rgba(91,21,115)',
+  color: 'rgba(255,255,255)',
 }));
 
 export function FaqAccordion() {
@@ -47,19 +57,27 @@ export function FaqAccordion() {
     <div>
       <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography>Collapsible Group Item #1</Typography>
+          <Typography>Где находится колледж?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
-            blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Улица Чкалова, 3 (3 этаж)
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>Collapsible Group Item #2</Typography>
+          <Typography>Какие документы я получу после окончания обучения?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Диплом государственного образца
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>Collapsible Group Item #3</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -69,8 +87,8 @@ export function FaqAccordion() {
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+      <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
           <Typography>Collapsible Group Item #3</Typography>
         </AccordionSummary>
         <AccordionDetails>
