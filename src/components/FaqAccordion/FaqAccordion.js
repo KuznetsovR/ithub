ea2aa06cc3e6126352dcfa611e43d02cfc410cd0
Polylykay <rubtsova-tanyal@mail.ru>
2022-03-28 20:47:17 +0500

    new accordion

--- src/components/FaqAccordion/FaqAccordion.js
+++ src/components/FaqAccordion/FaqAccordion.js
@@ -11,7 +11,8 @@ const Accordion = styled((props) => <MuiAccordion disableGutters elevation={0} s
     border: `1px solid ${theme.palette.divider}`,
     borderColor: 'rgba(91,21,115)',
     borderRadius: '10px',
-    backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255, .05)' : 'rgba(30,29,29)',
+    backgroundColor: theme.palette.mode === 'dark' ? 'rgba(255, 255, 255)' : 'rgba(30,29,29)',
+    color: 'rgba(255,255,255)',
     '&:not(:last-child)': {
       borderBottom: 0,
       color: 'rgba(255,255,255)',
@@ -36,18 +37,21 @@ const AccordionSummary = styled((props) => (
   },
   '& .MuiAccordionSummary-content': {
     marginLeft: theme.spacing(1),
-    color: 'rgba(167,29,216)',
+    color: 'rgba(175, 92, 204)',
+  },
+  '& .MuiAccordionSummary-expandIconWrapper': {
+    color: 'rgba(255,255,255)',
   },
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(91,21,115)',
+  borderTop: '2px solid rgba(91,21,115)',
   color: 'rgba(255,255,255)',
 }));
 
 export function FaqAccordion() {
-  const [expanded, setExpanded] = React.useState('panel1');
+  const [expanded, setExpanded] = React.useState('');
 
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
@@ -77,26 +81,61 @@ export function FaqAccordion() {
       </Accordion>
       <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
         <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography>Collapsible Group Item #3</Typography>
+          <Typography>Сколько стоит обучение?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
-            blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Стоимость обучения на 2022-2023 год: 15 000₽ в месяц
           </Typography>
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
         <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
-          <Typography>Collapsible Group Item #3</Typography>
+          <Typography>Есть ли бюджетные места в колледже?</Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet
-            blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          В нашем колледже бюджетных мест нет </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+        <AccordionSummary aria-controls="panel5d-content" id="panel5d-header">
+          <Typography>Как оплатить обучение?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Для оплаты обучения вам необходимо перейти на <a href="https://ithub.ru/payment/qiwi"> страницу оплаты.</a> <br/> <strong>Важно! </strong>Оплата образовательных услуг осуществляется ТОЛЬКО по безналичному расчету <br/> Через сервис ВТБ Онлайн, в том числе через приложение. Зайдите в раздел «Платежи и переводы», подраздел «Платежи в регионе Москва» и в строке поиска введите наш ИНН 9709063913, выберите наш колледж АНО ПО «ИТ ХАБ», введите: номер договора, ФИО обучающегося, назначение платежа, ФИО плательщика и сумму платежа. Подтвердите операцию. Готово! <br/> Возможна оплата через Тинькофф Банк! Через личный кабинет, в том числе через мобильное приложение. Зайдите в раздел «Платежи», подраздел «Образование» и в строке поиска введите наш ИНН 9709063913 или название ИТ ХАБ, выберите наш колледж АНО ПО «ИТ ХАБ», введите: номер договора, ФИО обучающегося, назначение платежа, ФИО плательщика и сумму платежа. Подтвердите операцию. Готово!
+ </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+        <AccordionSummary aria-controls="panel6d-content" id="panel6-header">
+          <Typography>Какие документы нужно предоставить для поступления?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+         - Оригинал или ксерокопию документов, удостоверяющих личность, гражданство <br/> - Оригинал или ксерокопию документа об образовании и (или) документа об образовании и о квалификации <br/> - 4 фотографии (3х4) анкета абитуриента (заполняется в приемной комиссии)
+</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
+        <AccordionSummary aria-controls="panel7d-content" id="panel7d-header">
+          <Typography>Можно ли оплатить учёбу материнским капиталом?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Да, можно. Мы составили <a href="https://ithub.ru/design/images/MatKapital.pdf"> инструкцию, как это сделать</a> 
+ </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion expanded={expanded === 'panel8'} onChange={handleChange('panel8')}>
+        <AccordionSummary aria-controls="panel8d-content" id="panel8d-header">
+          <Typography>Как получить налоговый вычет за обучение?</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          Действуйте по <a href="https://ithub.ru/design/images/nalogvichet.pdf"> инструкции</a> 
+ </Typography>
         </AccordionDetails>
       </Accordion>
     </div>
