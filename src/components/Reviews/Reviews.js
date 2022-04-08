7617d0c3c1144f0cb658d2654bb83d6d1546730f
KuznetsovR <abcruslan2004@mail.ru>
2022-04-08 11:34:20 +0500

    fixed photos in open day and changed css classes naming there

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Reviews.scss';
 import Slider from 'react-slick';
+import IvanPhoto from '../../assets/images/ivan.webp';
+import YaroslavPhoto from '../../assets/images/yaroslav.webp';
+import DefaultPhoto from '../../assets/images/ph.webp';
+import SergeiPhoto from '../../assets/images/sergei.webp';
 
 export const Reviews = () => {
   const settings = {
@@ -21,60 +25,80 @@ export const Reviews = () => {
           <div className={'slide'}>
             <div className="all-reviews-rectangles">
               <div className="student-photo-wrapper">
-                <div className="student-photo student-photo-1" />
+                <img src={IvanPhoto} alt="Ivan-Photo" className={'student-photo'} />
               </div>
               <div className="reviews-text">
                 <p>Иван (1 курс)</p>
-                <p className="reviews-text-2">Иван инициативный, любознательный. Хорошо владеет имеющимися умениями, с интересом и удовольствием осваивает новые практические сферы и прикладные инструменты. Умеет организовать собственную работу и процесс освоения новых навыков</p>
-                <p className="reviews-text-3">[ Директор регионального центра цифровой трансформации образования, ГАОУ ДПО ИРО Свердловской области Стариченко Е.Б. ]</p>
+                <p className="reviews-text-description">
+                  Иван инициативный, любознательный. Хорошо владеет имеющимися умениями, с интересом и
+                  удовольствием осваивает новые практические сферы и прикладные инструменты. Умеет
+                  организовать собственную работу и процесс освоения новых навыков
+                </p>
+                <p className="reviews-text-author">
+                  [ Директор регионального центра цифровой трансформации образования, ГАОУ ДПО ИРО
+                  Свердловской области Стариченко Е.Б. ]
+                </p>
               </div>
             </div>
           </div>
           <div className={'slide'}>
             <div className="all-reviews-rectangles">
               <div className="student-photo-wrapper">
-                <div className="student-photo student-photo-2" />
+                <img src={YaroslavPhoto} alt="Yaroslav-Photo" className={'student-photo'} />
               </div>
               <div className="reviews-text">
                 <p>Ярослав (2 курс)</p>
-                <p className="reviews-text-2">Ярослав очень быстро освоился в платформе. У него достаточно хороший багаж знаний. Задачи решает быстро. Смотрит чуть дальше. чем ожидалось, судя по вопросам, которые он задает и его рассуждениям.  Даже предложил несколько полезных изменений.</p>
-                <p className="reviews-text-3">[ Иваницкий А.М, директор компании Тандем ]</p>
+                <p className="reviews-text-description">
+                  Ярослав очень быстро освоился в платформе. У него достаточно хороший багаж знаний. Задачи
+                  решает быстро. Смотрит чуть дальше. чем ожидалось, судя по вопросам, которые он задает и его
+                  рассуждениям. Даже предложил несколько полезных изменений.
+                </p>
+                <p className="reviews-text-author">[ Иваницкий А.М, директор компании Тандем ]</p>
               </div>
             </div>
           </div>
           <div className={'slide'}>
             <div className="all-reviews-rectangles">
               <div className="student-photo-wrapper">
-                <div className="student-photo student-photo-4" />
+                <img src={SergeiPhoto} alt="Sergei-Photo" className={'student-photo'} />
               </div>
               <div className="reviews-text">
                 <p>Сергей (3 курс)</p>
-                <p className="reviews-text-2"> Сергей - в целом, доставляет результат и это главное. Креативен, хорош в поддержке команды. Всегда (!) находит решение. </p>
-                <p className="reviews-text-3">[ Ярослав Минуллин, компания Енерсофт ]</p>
+                <p className="reviews-text-description">
+                  Сергей - в целом, доставляет результат и это главное. Креативен, хорош в поддержке команды.
+                  Всегда (!) находит решение.
+                </p>
+                <p className="reviews-text-author">[ Ярослав Минуллин, компания Енерсофт ]</p>
               </div>
             </div>
           </div>
           <div className={'slide'}>
             <div className="all-reviews-rectangles">
               <div className="student-photo-wrapper">
-                <div className="student-photo student-photo-3" />
+                <img src={DefaultPhoto} alt="Pavel-Photo" className={'student-photo'} />
               </div>
               <div className="reviews-text">
                 <p>Павел (3 курс)</p>
-                <p className="reviews-text-2">У Павла удивительное сочетание рабочих и персональных качеств. Ответственный, нацеленный на результат, заряженный. Хорошая логика и критическое мышление.</p>
-                <p className="reviews-text-3">[ Ярослав Минуллин, компания Енерсофт ]</p>
+                <p className="reviews-text-description">
+                  У Павла удивительное сочетание рабочих и персональных качеств. Ответственный, нацеленный на
+                  результат, заряженный. Хорошая логика и критическое мышление.
+                </p>
+                <p className="reviews-text-author">[ Ярослав Минуллин, компания Енерсофт ]</p>
               </div>
             </div>
           </div>
           <div className={'slide'}>
             <div className="all-reviews-rectangles">
               <div className="student-photo-wrapper">
-                <div className="student-photo student-photo-3" />
+                <img src={DefaultPhoto} alt="Kirill-Photo" className={'student-photo'} />
               </div>
               <div className="reviews-text">
                 <p>Кирилл (3 курс)</p>
-                <p className="reviews-text-2">Кирилл - отличный командный игрок. Очень неплохое знание React, общая эрудированность. Может работать за рамками своей компетенции. Иногда утирал носы MID’ам из аутстаффинга.</p>
-                <p className="reviews-text-3">[ Ярослав Минуллин, компания Енерсофт ]</p>
+                <p className="reviews-text-description">
+                  Кирилл - отличный командный игрок. Очень неплохое знание React, общая эрудированность. Может
+                  работать за рамками своей компетенции. Иногда утирал носы MID’ам из аутстаффинга.
+                </p>
+                <p className="reviews-text-author">[ Ярослав Минуллин, компания Енерсофт ]</p>
               </div>
             </div>
           </div>
