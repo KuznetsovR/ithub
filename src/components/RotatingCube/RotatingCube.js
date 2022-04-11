b621991251b7892655f5164bf07f68a4d4e0aa36
KuznetsovR <abcruslan2004@mail.ru>
2022-04-11 18:33:05 +0500

    changed cube sizes

--- src/components/RotatingCube/RotatingCube.js
+++ src/components/RotatingCube/RotatingCube.js
@@ -7,7 +7,7 @@ export const RotatingCube = () =>{
     <div className="rotating-cube-wrapper">
       <div className="rotating-cube">
         <div className="rotating-cube-side rotating-cube-right">
-          <div className="rotating-cube-side-text">ITJOURNEY</div>
+          <div className="rotating-cube-side-text">IT JOURNEY</div>
         </div>
         <div className="rotating-cube-side rotating-cube-left">
           <div className="rotating-cube-side-text">ITHUB</div>
@@ -16,7 +16,7 @@ export const RotatingCube = () =>{
           <div className="rotating-cube-side-text">ThreeJS</div>
         </div>
         <div className="rotating-cube-side rotating-cube-bottom">
-          <div className="rotating-cube-side-text">3D</div>
+          <div className="rotating-cube-side-text">Ekat</div>
         </div>
         <div className="rotating-cube-side rotating-cube-front">
           <div className="rotating-cube-side-text"><img src={logo} className="rotating-cube-logo" alt="ithub-logo" /></div>
