d3869eadd15eeda067a5f11ed44224a362b4ad67
KuznetsovR <abcruslan2004@mail.ru>
2022-04-12 15:58:29 +0500

    customized file upload

--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FileUpload.scss';
 
 export const FileUpload = (props) => {
-  return <input type="file" className={'file-input'} onChange={(e) => props.onChange(e)} />;
+  const [selectedDocument, changeSelectedDocument] = useState('')
+  const handleFileInput = (e) => {
+    changeSelectedDocument(e.target.files[0].name)
+    props.onChange(e)
+  }
+  return (
+    <div className={'file-upload'}>
+      <label htmlFor={props.id}>{selectedDocument?selectedDocument: 'Выбрать документ'}</label>
+      <input type="file" className={'file-input'} onChange={(e) => handleFileInput(e)} id={props.id} />
+    </div>
+  );
 };
