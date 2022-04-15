fbb432beaccfef639e08470ea7194dbe0811da71
KuznetsovR <abcruslan2004@mail.ru>
2022-04-15 15:13:24 +0500

    made slider faster

--- src/components/Documents/Documents.js
+++ src/components/Documents/Documents.js
@@ -18,7 +18,7 @@ export const Documents = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 1300,
+    speed: 750,
     autoplaySpeed: 5000,
     arrows: false,
     lazyLoad: true,
